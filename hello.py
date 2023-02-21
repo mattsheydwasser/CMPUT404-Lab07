dcdfbbe9e078f4fcbaf10c8b0896ab7dcea97bd7
mattsheydwasser <mattsheydwasser@gmail.com>
2023-02-21 10:28:44 -0700

    finished lab code

--- hello.py
+++ hello.py
@@ -1,9 +1,62 @@
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/env python3
 
-@app.route("/")
-def hello():
-    return "Hello, world!"
+from flask_restful import Resource, Api, reqparse, abort
+from flask import Flask
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument("task")
+
+TODOs = {
+    1: {"task": "build an API"},
+    2: {"task": "?????"},
+    3: {"task": "profit"},
+}
+
+def abort_if_todo_not_found(todo_id):
+    if todo_id not in TODOs:
+        abort(404, message="TODO {} does not exist".format(todo_id))
+        
+def add_todo(todo_id):
+    args = parser.parse_args()
+    todo = {"task": args["task"]}
+    TODOs[todo_id] = todo
+    return todo
+
+
+class Todo(Resource):
+    """
+    Shows a todo item, allows for deletion of TODOs
+    """
+    
+    def get(self, todo_id):
+        abort_if_todo_not_found(todo_id)
+        return TODOs[todo_id]
+    
+    def delete(self, todo_id):
+        abort_if_todo_not_found(todo_id)
+        del TODOs[todo_id]
+        return "", 204
+    
+    def put(self, todo_id):
+        return add_todo(todo_id), 201
+    
+class TodoList(Resource):
+    """
+    Gets list of all TODOs, can POST to add new ones
+    """
+    
+    def get(self):
+        return TODOs
+    
+    def post(self):
+        todo_id = max(TODOs.keys()) + 1
+        return add_todo(todo_id), 201
+
+api.add_resource(Todo, "/todos/<int:todo_id>")
+api.add_resource(TodoList, "/todos")
 
 if __name__ == "__main__":
     app.run(debug=True)
